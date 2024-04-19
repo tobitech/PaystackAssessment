91bfeae8f63b82001acf89bf5098469c07b0f41a
Tobi Omotayo <tobitech@ymail.com>
2024-04-19 12:03:18 +0100

    project setup and initial commit.

---
+++ PaystackAssessment/RootView.swift
@@ -0,0 +1,24 @@
+//
+//  RootView.swift
+//  PaystackAssessment
+//
+//  Created by Oluwatobi Omotayo on 19/04/2024.
+//
+
+import SwiftUI
+
+struct RootView: View {
+	var body: some View {
+		VStack {
+			Image(systemName: "globe")
+				.imageScale(.large)
+				.foregroundStyle(.tint)
+			Text("Hello, world!")
+		}
+		.padding()
+	}
+}
+
+#Preview {
+	RootView()
+}
