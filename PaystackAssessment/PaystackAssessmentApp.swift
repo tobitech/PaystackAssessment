0560e7c47bd5e8569ec524e56d66b1cb4e02410d
Tobi Omotayo <tobitech@ymail.com>
2024-04-19 12:02:07 +0100

    Initial Commit

---
+++ PaystackAssessment/PaystackAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaystackAssessmentApp.swift
+//  PaystackAssessment
+//
+//  Created by Oluwatobi Omotayo on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PaystackAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
