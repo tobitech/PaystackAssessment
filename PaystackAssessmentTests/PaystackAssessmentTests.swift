bf29ff2cab42e4d756ba71a4b29183080a02cd18
Tobi Omotayo <tobitech@ymail.com>
2024-04-20 23:58:35 +0100

    add a test target to the project

---
+++ PaystackAssessmentTests/PaystackAssessmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaystackAssessmentTests.swift
+//  PaystackAssessmentTests
+//
+//  Created by Oluwatobi Omotayo on 20/04/2024.
+//
+
+import XCTest
+@testable import PaystackAssessment
+
+final class PaystackAssessmentTests: XCTestCase {
+	
+	func testBasics() async throws {
+		
+	}
+	
+}
